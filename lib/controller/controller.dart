d2486dda236151423222ce08720fd544c0c89e52
CimolKentang <cimolkentang245@gmail.com>
2024-02-08 15:31:18 +0700

    add adding method

--- lib/controller/controller.dart
+++ lib/controller/controller.dart
@@ -1,9 +1,15 @@
 import 'package:get/get.dart';
+import 'package:getx_bla/global/kelas_pelajaran.dart';
 
 class ListController extends GetxController {
+  Rx<List<Pelajaran>> pelajaran2 = Rx<List<Pelajaran>>([]);
   final name = ''.obs;
 
   void fungsi(String teks) {
     name.value = teks;
+  }
+
+  void tambah(Pelajaran pelajaran) {
+    pelajaran2.value.add(pelajaran);
   }
 }
---
+++ lib/global/kelas_pelajaran.dart
@@ -0,0 +1,15 @@
+class Pelajaran {
+  final int id;
+  final String name;
+  final int jumlahSKS;
+  final int jumlahHalaman;
+  final String pengajar;
+
+  Pelajaran({
+    required this.id,
+    required this.name,
+    required this.jumlahSKS,
+    required this.jumlahHalaman,
+    required this.pengajar,
+  });
+}
