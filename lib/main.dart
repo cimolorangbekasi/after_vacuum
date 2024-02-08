9b5eada26b82c159803de769a194525e7ffa36a9
umar izzuddin <ibnabdilazeez14@gmail.com>
2024-02-08 16:11:01 +0700

    adding pre-interfce in detail

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:getx_bla/global/kelas_pelajaran.dart';
+import 'package:getx_bla/screen/home/detail/detail.dart';
 import 'package:getx_bla/screen/home/home.dart';
 
 // hahaha ane hapus semua
@@ -18,7 +20,14 @@ class Aplikasi extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ), // bisa juga ThemeData(appBarTheme: AppBarTheme(backgroundColor: Colors.blue))
-      home: const Home(),
+      // home: const Home(),
+      home: Detail(
+          pelajaran: Pelajaran(
+              id: 4,
+              name: 'nangis',
+              jumlahSKS: 5,
+              jumlahHalaman: 6723,
+              pengajar: 'Prabowo')),
     );
   }
 }
