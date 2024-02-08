8a9d79c41094bb1f1c1c0deca655979e3881e8a3
umar izzuddin <ibnabdilazeez14@gmail.com>
2024-02-08 16:27:42 +0700

    styled detail

--- lib/screen/home/detail/detail.dart
+++ lib/screen/home/detail/detail.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:getx_bla/global/kelas_pelajaran.dart';
+import 'package:getx_bla/global/static_styles.dart';
 
 class Detail extends StatelessWidget {
   const Detail({super.key, required this.pelajaran});
@@ -9,14 +10,61 @@ class Detail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(height: MediaQuery.of(context).size.height / 4),
-          Text(pelajaran.name),
-          Text(pelajaran.pengajar),
-          Text(pelajaran.jumlahHalaman.toString()),
-          Text(pelajaran.jumlahSKS.toString()),
-        ],
+      backgroundColor: const Color.fromARGB(255, 202, 197, 182),
+      body: Padding(
+        padding: EdgeInsets.symmetric(
+            horizontal: MediaQuery.of(context).size.width / 16),
+        child: Column(
+          children: [
+            SizedBox(height: MediaQuery.of(context).size.height / 8),
+            Text(
+              pelajaran.name,
+              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Text('pengajar : ${pelajaran.pengajar}',
+                      style: const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white)),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text('${pelajaran.jumlahHalaman.toString()} pages',
+                        style: const TextStyle(
+                            fontSize: 14, fontWeight: FontWeight.w300)),
+                    Text('${pelajaran.jumlahSKS.toString()} SKS',
+                        style: const TextStyle(
+                            fontSize: 14, fontWeight: FontWeight.w300)),
+                  ],
+                ),
+              ],
+            ),
+            const Spacer(),
+            ElevatedButton(
+              onPressed: () => (),
+              style: StaticStyles.buttonDark(),
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Tambahkan ke Keranjang',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height / 16),
+          ],
+        ),
       ),
     );
   }
